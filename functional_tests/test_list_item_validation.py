0cdb8b78dcadb5352546412f6c3e83b2a599e7f3
Matthew Stibbard <matthewstibbard@gmail.com>
2017-06-17 21:42:39 +0100

    FT updated for javascript

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -65,7 +65,7 @@ class ItemValidationTest(FunctionalTest):
         ))
 
     def test_error_messages_are_cleared_on_input(self):
-        # Edith starts a list and causes a validation error
+        # Edith starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
