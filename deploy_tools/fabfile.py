8ac3cd6ae1b5af18ea21ac1b08f5ca167fb120a9
Matthew Stibbard <matthewstibbard@gmail.com>
2017-06-18 13:42:30 +0100

    Fixed fabfile current_commit issue

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -29,7 +29,7 @@ def _get_latest_source(source_folder):
     else:
         run(f'git clone {REPO_URL} {source_folder}')
     current_commit = local(
-        "git log -n l --format=%H", capture=True)
+        "git log -n 1 --format=%H", capture=True)
     run(f'cd {source_folder} && git reset --hard {current_commit}')
 
 
